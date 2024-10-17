27ae3c468ac2cd0d7e2b9951cdf2b66d05b3d924
ALI254746! <hojiakbarmuhammad77@gmail.com>
2024-10-17 15:53:59 +0500

    first commit

---
+++ javascript_dars1.js
@@ -0,0 +1,105 @@
+"use strict";
+// let number=5;
+// console.log(number);
+// let name="ali";
+// console.log(name);
+// let bool=true;
+// console.log(bool);
+// let str1='salom dunyo';
+// let str2='how are you';
+// let katta=str2.toUpperCase();
+// let uzunlik=str1.length;
+// let katta1=str1.toLowerCase();
+
+// console.log(katta);
+// console.log(katta1);
+// console.log(uzunlik);
+// let str3=str1 + " " +str2;
+// console.log(str3);
+// let now =new Date();
+//alert(now);
+//let hozir=new Date();
+//console.log(hozir);
+//let yil = hozir.getFullYear();
+//let oy = hozir.getMonth(); // 0 dan 11 gacha (0 - yanvar, 11 - dekabr)
+//let kun = hozir.getDate();
+//let soat = hozir.getHours();
+//let daqiqa = hozir.getMinutes();
+//let sekund = hozir.getSeconds();
+
+//console.log(`Yil: ${yil}, Oy: ${oy + 1}, Kun: ${kun}, Soat: ${soat}, Daqiqa: ${daqiqa}, Sekund: ${sekund}`);
+//let qiymat=prompt("ismingiz nima=");
+//console.log(qiymat);
+//let foydalanuvchi=null;
+//if (qiymat===null){
+   // console.log("foydalanuvchi mavjud emas.");
+//}
+//else{
+//    console.log("foydalanuvchi bor");
+//}
+//object
+
+//let person={
+    //age:27,
+    //isim:'hojiakbar',
+    //kasb:'softwer',
+  //  male:true,
+//};
+//console.log(person);
+//array
+//let arr=['phone ', 'mouse','speed',]
+//console.log(arr)
+//arr[0]='desktop'
+//console.log(arr);
+// let ism='ali';
+// let n=ism +' '+ "salom";
+// console.log(n);
+// let na=`salom ${ism} !`;
+// console.log(na);
+// let a=10,b=5;
+// const z=10;
+// --a;
+// console.log(a);
+// b++;
+// console.log(b);
+// a+=b;
+// console.log(a);
+
+
+// let a,b;
+
+// function EKUK(a,b){
+//     for(let i=1; i<a*b; i++){
+//         if(i%a==0 && i%b==0){
+//             return i;
+//         }
+//     }
+ 
+// }
+
+// document.getElementById('EKUK').innerText=EKUK(15,25)
+// console.log(EKUK(25,15))
+// document.addEventListener('DOMContentLoaded', function () {
+//     let juftson = prompt("Son kirit =");
+//     if (juftson % 2 == 0) {
+//         console.log("Son juft son");
+//         document.getElementById('juft').innerText = "Son juft son".toUpperCase(); // HTML elementiga natija yozamiz
+//     } else {
+//         console.log("Son toq son");
+//         document.getElementById('toq').innerText = "Son toq son"; // HTML elementiga natija yozamiz
+//     }
+// });
+// let age=prompt("yoshing nechida=");
+// switch(age){
+//     case 18:
+//         alert('sizni yoshingiz 18 da');
+//         break
+//         case 50:
+//             alert("sizni yoshingiz 50 da");
+//             break
+//             default:
+//                 alert('siz yoshingiz noma\'lum);
+// }
+
+
+
